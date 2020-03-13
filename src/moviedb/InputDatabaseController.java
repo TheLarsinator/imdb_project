238d67704a9ef6f58b1cc5b7b1773a074ac5d4e4
Lars van der Lee <theLarsinatormodding@hotmail.com>
2020-03-13 16:35:02 +0100

    Give persons roles in movies

--- src/moviedb/InputDatabaseController.java
+++ src/moviedb/InputDatabaseController.java
@@ -1,6 +1,8 @@
 package moviedb;
 
+import moviedb.tables.ActorInMedia;
 import moviedb.tables.Person;
+import moviedb.tables.ProducerInMedia;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,7 +15,7 @@ public class InputDatabaseController extends DatabaseController {
         super();
     }
 
-    public boolean createPerson(BufferedReader reader, OutputDatabaseController outCtrl){
+    public Person createPerson(BufferedReader reader, OutputDatabaseController outCtrl){
         String name;
         String birthDate;
         String nationality;
@@ -23,17 +25,50 @@ public class InputDatabaseController extends DatabaseController {
         name = getUserInput(reader);
 
         //Check if this person already exists, should be primary key but too late to change that now
-        Person pers = outCtrl.findPersonByName(name);
-        if(pers.getNationality() != null){
+        Person person = outCtrl.findPersonByName(name);
+        if(person.getNationality() != null){
             System.out.println("Person already exists. Choose new option");
-            return true;
+            return person;
         }
 
         System.out.println("Insert person birth date:");
         birthDate = getUserInput(reader);
         System.out.println("Insert person nationality:");
         nationality = getUserInput(reader);
+        person = new Person(birthDate, name, nationality);
+        createEntry(person);
+        return person;
+    }
 
-        return createEntry(new Person(birthDate, name, nationality));
+    public void givePersonActingRoleInMediaItem(BufferedReader reader, OutputDatabaseController outCtrl, int mediaItemID){
+        Person actor = createPerson(reader, outCtrl);
+
+        System.out.println("What is " + actor.getName() + "'s role?");
+        String role = getUserInput(reader);
+
+        ActorInMedia aim = new ActorInMedia(actor.getName(), mediaItemID, role);
+        createEntry(aim);
+    }
+
+    public void givePersonProductionRoleInMediaItem(BufferedReader reader, OutputDatabaseController outCtrl, int mediaItemID){
+        Person producer = createPerson(reader, outCtrl);
+
+        System.out.println("What is " + producer.getName() + "'s role?");
+        int role = 0;
+        while(role == 0) {
+            System.out.println("1: Director");
+            System.out.println("2: Writer");
+
+            int a = Integer.parseInt(getUserInput(reader));
+            if(a == 1 || a == 2){
+                role = a;
+            }
+            else{
+                continue;
+            }
+        }
+
+        ProducerInMedia pim = new ProducerInMedia(producer.getName(), mediaItemID, role == 1 ? "Director" : "Writer" );
+        createEntry(pim);
     }
 }
